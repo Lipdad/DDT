ef889b64461c427e1361350eb0ad89dce987b83c
Lipdad <danhtuan2k3@gmail.com>
2021-11-05 16:43:12 +0700

    Signed-off-by: Lipdad <danhtuan2k3@gmail.com>

--- 14.Tổng kết/index.js
+++ 14.Tổng kết/index.js
@@ -10,6 +10,24 @@ var paddleWidth = 75;
 var paddleX = (canvas.width - paddleWidth) / 2;
 var rightPressed = false;
 var leftPressed = false;
+var brickRowCount = 8;
+var brickColumnCount = 4;
+var brickWidth = 80;
+var brickHeight = 25;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+var score = 0;
+var lives = 5;
+
+var bricks = [];
+for (c = 0; c < brickColumnCount; c++) {
+    bricks[c] = [];
+    for (r = 0; r < brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0, status: 1 };
+    }
+}
+
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
@@ -28,52 +46,119 @@ function keyUpHandler(e) {
         leftPressed = false;
     }
 }
-
+if (rightPressed && paddleX < canvas.width - paddleWidth) {
+    paddleX += 7;
+} else if (leftPressed && paddleX > 0) {
+    paddleX -= 7;
+}
+// 
+function collisionDetection() {
+    for (c = 0; c < brickColumnCount; c++) {
+        for (r = 0; r < brickRowCount; r++) {
+            var b = bricks[c][r];
+            if (b.status == 1) {
+                if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                    score++;
+                    if (score == brickRowCount * brickColumnCount) {
+                        alert("CHÚC MỪNG!Bạn đã chiến thắng");
+                        document.location.reload();
+                    }
+                }
+            }
+        }
+    }
+}
+// Hàm vẽ bóng
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-    ctx.fillStyle = "black";
+    ctx.fillStyle = "#1E90FF";
     ctx.fill();
     ctx.closePath();
 }
-
+// Hàm vẽ bar
 function drawPaddle() {
     ctx.beginPath();
     ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = "blue";
+    ctx.fillStyle = "#008B8B";
     ctx.fill();
     ctx.closePath();
 }
+// Hàm vẽ gạch
+function drawBricks() {
+    for (c = 0; c < brickColumnCount; c++) {
+        for (r = 0; r < brickRowCount; r++) {
+            if (bricks[c][r].status == 1) {
+                var brickX = (r * (brickWidth + brickPadding)) + brickOffsetLeft;
+                var brickY = (c * (brickHeight + brickPadding)) + brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#B22222";
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
 
-
-
-
-
+// hàm tính điểm
+function drawScore() {
+    ctx.font = "18px Times";
+    ctx.fillStyle = "#000000";
+    ctx.fillText("Score: " + score, 8, 20);
+}
+// hàm tính mạng
+function drawLives() {
+    ctx.font = "18px Times";
+    ctx.fillStyle = "#000000";
+    ctx.fillText("Lives: " + lives, canvas.width - 65, 20);
+}
+// hàm va chạm bóng 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
     drawBall();
     drawPaddle();
+    drawScore();
+    drawLives();
+    collisionDetection();
+
     if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
     if (y + dy < ballRadius) {
         dy = -dy;
+    } else if (y + dy > canvas.height - ballRadius) {
+        if (x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        } else {
+            lives--;
+            if (!lives) {
+                alert("Điểm của bạn là" + "userScore");
+                document.location.reload();
+            } else {
+                x = canvas.width / 2;
+                y = canvas.height - 30;
+                dx = 3;
+                dy = -3;
+                paddleX = (canvas.width - paddleWidth) / 2;
+            }
+        }
     }
-    if (y + dy > canvas.height - ballRadius) {
-        dy = -dy;
-    }
+    // hàm di chuyển thanh bar
     if (rightPressed && paddleX < canvas.width - paddleWidth) {
         paddleX += 7;
     } else if (leftPressed && paddleX > 0) {
         paddleX -= 7;
     }
+
     x += dx;
     y += dy;
+    requestAnimationFrame(draw);
 }
-setInterval(draw, 10);
 
-
-
-function collisionDectection() {
-
-}
+draw();
